9b6849898f80c50d65e2f0ca2e4b2ee544ed62e2
danillo.sdd <danillo.sdd@hotmail.com>
2026-06-14 00:32:49 -0300

    fix(mobile): alterar ip da api para rodar em celular fisico

--- app-mobile/src/servicos/api.js
+++ app-mobile/src/servicos/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Para emulador Android use 10.0.2.2; para celular fisico troque para o IP da sua maquina.
 const api = axios.create({
-  baseURL: 'http://10.0.2.2:8082/mcontroller',
+  baseURL: 'http://192.168.15.23:8082/mcontroller',
 });
 
 export default api;
