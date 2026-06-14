964a5c55903c458961675618467f2151cf9e9001
danillo.sdd <danillo.sdd@hotmail.com>
2026-06-13 23:57:23 -0300

    fix: corrige API Key original extraída do console

--- app-mobile/src/servicos/firebase.js
+++ app-mobile/src/servicos/firebase.js
@@ -6,12 +6,13 @@ import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } f
 // 2. Adicione um app Web e ative a autenticação por Email/Senha
 // 3. Copie o firebaseConfig e cole aqui:
 const firebaseConfig = {
-  apiKey: "AIzaSyDP1_ktapCAZYRKEMCegaouGLaxFs9tsRY",
+  apiKey: "AIzaSyDP1_ktaPCAZYRKEMCegaouGlaXFs9tsRY",
   authDomain: "lab-track-ueg.firebaseapp.com",
   projectId: "lab-track-ueg",
   storageBucket: "lab-track-ueg.firebasestorage.app",
   messagingSenderId: "635888953371",
-  appId: "1:635888953371:web:a4340501470f7fe000f0be"
+  appId: "1:635888953371:web:a4340501470f7fe000f0be",
+  measurementId: "G-C7JQ9RM8KH"
 };
 
 let app;
